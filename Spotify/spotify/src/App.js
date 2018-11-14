3f62002965d0f9fae6af502a5803c62a07fef8a3
Robert Kindwall <kindwall@kth.se>
2018-11-14 19:18:19 +0100

    Add project

---
+++ Spotify/spotify/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="textbox">
+          Playlist code:
+        </div>
+        <div className="username">
+          AE2X5
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
